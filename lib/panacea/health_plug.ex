cedffcede4a34fa296eed0c3aebbe5d7d0d6143c
Poncho Martinez <poncho.811@gmail.com>
2018-05-24 18:55:34 -0500

    Fix health endpoint on composite path

--- lib/panacea/health_plug.ex
+++ lib/panacea/health_plug.ex
@@ -8,12 +8,15 @@ defmodule Panacea.HealthPlug do
   import Plug.Conn
 
   def init(opts), do: opts
-
+  
   def call(conn = %Plug.Conn{path_info: [], method: "GET"}, opts) do
     conn
   end
-  def call(conn = %Plug.Conn{path_info: [path], method: "GET"}, opts) do
-    if path == Application.get_env(:panacea, :endpoint) do
+
+  def call(conn = %Plug.Conn{path_info: path_info, method: "GET"}, opts) do
+    path = Enum.join(path_info, "/")
+
+    if Enum.member?(Application.get_env(:panacea, :endpoint, []), path) do
       conn
       |> put_resp_content_type("application/json", "UTF-8")
       |> send_resp(200, build_health_response())
