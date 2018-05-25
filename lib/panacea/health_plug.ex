2b734e482344a8f51a0a1254a7fbc94b01514eba
Poncho Martinez <poncho.811@gmail.com>
2018-05-24 19:04:58 -0500

    Health endpoint config can be list or String

--- lib/panacea/health_plug.ex
+++ lib/panacea/health_plug.ex
@@ -8,15 +8,11 @@ defmodule Panacea.HealthPlug do
   import Plug.Conn
 
   def init(opts), do: opts
-  
-  def call(conn = %Plug.Conn{path_info: [], method: "GET"}, opts) do
-    conn
-  end
 
   def call(conn = %Plug.Conn{path_info: path_info, method: "GET"}, opts) do
     path = Enum.join(path_info, "/")
 
-    if Enum.member?(Application.get_env(:panacea, :endpoint, []), path) do
+    if is_health_path(Application.get_env(:panacea, :endpoint), path) do
       conn
       |> put_resp_content_type("application/json", "UTF-8")
       |> send_resp(200, build_health_response())
@@ -26,7 +22,17 @@ defmodule Panacea.HealthPlug do
     end
   end
 
-  def build_health_response do
+  defp is_health_path(nil, path) do
+    false
+  end
+  defp is_health_path(health_paths, path) when is_list(health_paths) do
+    Enum.member?(health_paths, path)
+  end
+  defp is_health_path(health_path, path) when is_binary(health_path) do
+    health_path == path
+  end
+
+  defp build_health_response do
     %{
       "success" => true,
       "timestamp" => DateTime.utc_now
